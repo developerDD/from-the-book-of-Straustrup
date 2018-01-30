d37e109b82e9b340f227c154b4c111fbb69e1413
developerDD <developerDD@ukr.net>
2018-01-30 16:18:54 +0200

    поиск повторяющихся слов в потоке

---
+++ Exercises/Exercises/Source.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include<vector>
+#include<string>
+//поиск слов которые повторяются и вывод слова и количества (с помощью создания структуры)
+
+using namespace std;
+
+struct Pair
+{
+	string name;
+	double vel;
+
+};
+vector<Pair> pairs;
+
+double& value(const string& s)
+{
+	for (size_t i = 0; i < pairs.size(); i++)
+	{
+		if (pairs[i].name == s)
+		{
+			return pairs[i].vel;
+		}
+	}
+	Pair p = { s, 0 };
+	pairs.push_back(p);
+	return pairs[pairs.size() - 1].vel;
+
+}
+void main()
+{
+	
+	string buff;
+
+	while (cin >> buff&&buff != "1")
+	{
+		value(buff)++;
+
+	}
+	for (vector<Pair>::const_iterator p = pairs.begin(); p != pairs.end(); ++p)
+	{
+		cout << p->name << ":" << p->vel << "\n";
+	}
+
+}
