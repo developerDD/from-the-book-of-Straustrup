749c050b5817630992e604b887f103152c8fea2b
developerDD <developerDD@ukr.net>
2018-02-02 15:15:07 +0200

    5.9.9

--- Exercises/Exercises/Source.cpp
+++ Exercises/Exercises/Source.cpp
@@ -27,6 +27,33 @@
 		return pairs[pairs.size() - 1].vel;
 
 	}
+
+	void f(int* a, int* b)
+	{
+		/*int t;
+		t = *a;
+		*a = *b;
+		*b = t;*/
+		//whit out t
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+	}
+	void f2(int& a, int& b)
+	{
+		a = a + b;
+		b = a - b;
+		a = a - b;
+	}
+	void prints(string* arrstr)
+	{
+		
+			for (int i = 0; i < 3; i++)
+			{
+				cout << *(arrstr + i) << "/" << arrstr[i] << endl;
+			}
+		
+	}
 	void main()
 	{
 
@@ -42,23 +69,51 @@
 		cout << p->name << ":" << p->vel << "\n";
 		}*/
 		// initialize the variables
-		char D = 'D';
-		char* pch = &D;
-		int arr[10] = {};
-		string* pstr = new string[10];
-		char** ppch = &pch;
-		const int ci = 1;
-		const int* pci = &ci;
-		const int* const cpci = pci;
-		//use typedef
-		typedef unsigned char  uchar;
-		typedef const unsigned char cuchar; 
-		typedef int* pint;		//pointer on int 
-		typedef char** ppchar; //pointer on pointer on char
-		typedef char* parrchar; //array pointer
+		//char D = 'D';
+		//char* pch = &D;
+		//int arr[10] = {};
+		//string* pstr = new string[10];
+		//char** ppch = &pch;
+		//const int ci = 1;
+		//const int* pci = &ci;
+		//const int* const cpci = pci;
+		////use typedef
+		//typedef unsigned char  uchar;
+		//typedef const unsigned char cuchar; 
+		//typedef int* pint;		//pointer on int 
+		//typedef char** ppchar; //pointer on pointer on char
+		//typedef char* parrchar; //array pointer
+		//
+		//pint array_pointer[7];
+		//int** ppint = array_pointer;
+		//ppint[8];
+
+		//int a = 1;
+		//int b = 2;
+		//int* pa; int* pb;
+		//pa = &a;
+		//pb = &b;
+		//cout << a <<" "<< b << "\n";
+		//f(pa, pb);
+		//cout << a << " " << b << "\n";
+		//f2(a, b);
+		//cout << a << " " << b << "\n";
+
+		//char str[] = "a short string";
+		//int intv[] = { 1, 2, 3 };
+		//cout << sizeof(str) << " " << sizeof(str) / sizeof(char) << "\n";
+		//cout << sizeof(intv) << " " << sizeof(intv) / sizeof(int);
+		////use pointer and idex
+		//for (size_t i = 0; i < sizeof(str); i++)
+		//{
+		//	cout << *(str + i) << "\\"<<str[i]<<"\n";
+		//}
+		//transfer array in function
+		string arrstr[] = { "a", "s", "c" };
 		
-		pint array_pointer[7];
-		int** ppint = array_pointer;
-		ppint[8];
+		prints(arrstr);
+
 		
 	}
+
+	
