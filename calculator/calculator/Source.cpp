585d71ebea11f44cb7fa38dd6eb5c2c803629bb7
developerDD <developerDD@ukr.net>
2018-02-06 23:22:50 +0200

    versoin 0.3

--- calculator/calculator/Source.cpp
+++ calculator/calculator/Source.cpp
@@ -1,12 +1,9 @@
 #include<iostream>
-#include<vector>
-#include<map>
 #include<string>
+#include<map>
 
 using namespace std;
-
-//
-enum Token_value 
+enum Token_value
 {
 	NAME, NUMBER, END, PLUS = '+', MINUS = '-', MUL = '*', DIV = '/', PRINT = ';', ASSIGN = '=', LP = '(', RP = ')'
 };
@@ -14,6 +11,7 @@ Token_value curr_tok = PRINT;
 //addition/subtraction
 double term(bool);
 double prim(bool);
+Token_value get_token();
 double error(const string&);
 double expr(bool get)
 {
@@ -49,7 +47,7 @@ double term(bool get)
 		case DIV:
 			if (double d = prim(true))
 			{
-				left / d;
+				left /= d;
 				break;
 			}
 			return error("divide by 0");
@@ -60,6 +58,43 @@ double term(bool get)
 	}
 }
 
+double nuber_value;
+string string_value;
+map<string, double> table;
+
+double prim(bool get)
+{
+	if (get)
+	{
+		get_token();
+	}
+	switch (curr_tok)
+	{
+	case NUMBER:
+		double v = nuber_value;
+		get_token();
+		return v;
+	case NAME:
+		double& v = table[string_value];
+		if (get_token()==ASSIGN)
+		{
+			v = expr(true);
+		}
+		return v;
+	case MINUS:
+		return -prim(true);
+	case LP:
+		double e = expr(true);
+		if (curr_tok!=RP)
+		{
+			return error("')' excepted");
+		}
+		get_token(); //skip ')'
+		return e;
+	default:
+		return error("primary excepted");
+	}
+}
 //error messages
 double error(const string& s)
 {
@@ -100,9 +135,72 @@ Token_value get_token()
 	case '=':
 		return curr_tok = Token_value(ch);
 		break;
+	case '0':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	case '1':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	case '2':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	case '3':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	case '4':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	case '5':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	case '6':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	case '7':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	case '8':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	case '9':
+		cin.putback(ch);
+		cin >> nuber_value;
+		return curr_tok = NUMBER;
+	default:
+		if (isalpha(ch))
+		{
+			string_value = ch;
+			while (cin.get(ch)&&isalnum(ch))
+			{
+				string_value.push_back(ch);
+			}
+			cin.putback(ch);
+			return curr_tok = NAME;
 		}
+		error("bad token");
+		curr_tok = PRINT;
+	}
 }
 void main()
 {
+	table["pi"] = 3.1415926535897932385;
+	table["å"] = 2.7182818284590452354;
 
+	while (cin)
+	{
+		get_token();
+		if (curr_tok == END) break;
+		if (curr_tok == PRINT) continue;
+		cout<< expr(false) << ' \n ';
+	}
+	
 }
